7c90d2d0ff7f56c08a6f08dd3e276f9babdea8e9
Hazim Sulaiman <hazim897@gmail.com>
2022-08-11 03:18:31 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/", (req, res) => {
+  const endpoint = req.query["endpoint"];
+  const url = `https://fantasy.premierleague.com/api/${endpoint}`;
+  console.log(url);
+  request(url).pipe(res);
+});
+
+app.listen(port);
